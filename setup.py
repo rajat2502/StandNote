30a156273b105de2c8bb7eb9fc78b3e9bc934ac4
geekquad <adityaastranaut@gmail.com>
2020-11-30 03:33:51 +0530

    Updating

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
-  name = 'YOURPACKAGENAME',         # How you named your package folder (MyLib)
-  packages = ['YOURPACKAGENAME'],   # Chose the same as "name"
+  name = 'standsum',         # How you named your package folder (MyLib)
+  packages = ['standsum'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'An extractive text summarizer using Cosine Methods in NLTK',   # Give a short description about your library
