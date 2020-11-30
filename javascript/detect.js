1998f90ad58fbdcf8973875ff2e88e45ddd75b04
Rajat Verma <rajatverma5885045@gmail.com>
2020-11-30 18:20:27 +0530

    unnecessary files removed

--- javascript/detect.js
+++
@@ -1,5 +0,0 @@
-const iframe = document.createElement('iframe');
-iframe.style.display = 'none';
-iframe.src = chrome.extension.getURL('../html/audiosources.html');
-iframe.allow = 'microphone';
-document.body.appendChild(iframe);
