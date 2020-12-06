0bd16ad131b3dfec6ae1253bda03e172dd76d6fd
PragatiVerma18 <itispragativerma@gmail.com>
2020-12-06 20:22:23 +0530

    Deploy fiels added

--- standnote/settings.py
+++ standnote/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
