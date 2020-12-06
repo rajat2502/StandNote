327e30bed3649b50870ef2c120254c4b97b11f24
PragatiVerma18 <itispragativerma@gmail.com>
2020-12-06 11:06:54 +0530

    Initialised React App

--- javascript/textEditor.js
+++
@@ -1,9 +0,0 @@
-let meetingText = window.text;
-const textarea = document.getElementById('meetingText');
-
-document.getElementById('score').innerText = window.confidenceScore;
-textarea.value = meetingText;
-
-textarea.addEventListener('keyup', (e) => {
-  meetingText = e.target.value;
-});
