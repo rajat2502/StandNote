355b32546d7fb9418aae9be5d56d87e623a31c68
PragatiVerma18 <itispragativerma@gmail.com>
2020-12-07 23:17:17 +0530

    added accounts

--- standnote/urls.py
+++ standnote/urls.py
@@ -7,6 +7,7 @@ from social_login.views import FacebookLogin, GoogleLogin
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^summarizer/$', views.MyView.as_view(), name="summarizer"),
-    path('rest-auth/facebook/', FacebookLogin.as_view()),
-    path('rest-auth/google/', GoogleLogin.as_view())
+    path('rest-auth/facebook/', FacebookLogin.as_view(), name='fb_login'),
+    path('rest-auth/google/', GoogleLogin.as_view(), name='google_login'),
+    url(r'^accounts/', include('allauth.urls'), name='socialaccount_signup'),
 ]
