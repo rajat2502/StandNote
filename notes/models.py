ec2707f7fde0795b0a32bedf3efbc0df63c1b588
PragatiVerma18 <itispragativerma@gmail.com>
2020-12-10 02:34:43 +0530

    Added duration

--- notes/models.py
+++ notes/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+# from summarizer.views import SummariserCosine
 from django.contrib.auth.models import User
 User._meta.get_field('email')._unique = True
 
 
 class Note(models.Model):
-    email = models.OneToOneField(
+    email = models.ForeignKey(
         User, on_delete=models.CASCADE, to_field='email')
     title = models.CharField(max_length=50)
     content = models.TextField(blank=True)
@@ -15,3 +16,9 @@ class Note(models.Model):
 
     def __str__(self):
         return self.title
+
+    # def get_markdown(self):
+    #     summ = SummariserCosine()
+    #     summerised_text, ranked_sentences = summ.generate_summary(
+    #         self.content, 6)
+    #     return summerised_text
