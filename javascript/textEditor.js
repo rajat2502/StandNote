652498f0acc2b5c16a2ede07c64af1a6c80c9958
Rajat Verma <rajatverma5885045@gmail.com>
2020-12-10 16:59:09 +0530

    feat: summarizer api integrate

--- javascript/textEditor.js
+++ javascript/textEditor.js
@@ -22,46 +22,67 @@ textarea.addEventListener('keyup', (e) => {
   meetingText = e.target.value;
 });
 
+async function getSummarizedText() {
+  const body = { email, text: meetingText, max_sentences: 2 };
+  console.log(body);
+
+  const res = await fetch('http://standnote.herokuapp.com/summarizer/', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  });
+  const data = await res.json();
+  console.log(data);
+
+  return data;
+}
+
 document
   .getElementById('submit-meeting-details')
-  .addEventListener('submit', (e) => {
-    e.preventDefault();
+  .addEventListener('submit', async (e) => {
+    try {
+      e.preventDefault();
 
-    document.getElementById('sendText').disabled = true;
+      document.getElementById('error').innerText = '';
+      document.getElementById('sendText').disabled = true;
 
-    title = document.getElementById('title').value;
+      const data = await getSummarizedText();
 
-    const body = {
-      email,
-      duration: meetingDuration,
-      title,
-      content: meetingText,
-      markdown: '',
-      score: window.confidenceScore,
-    };
+      title = document.getElementById('title').value;
 
-    console.log(body);
+      const body = {
+        email,
+        duration: meetingDuration,
+        title,
+        content: meetingText,
+        markdown: data.summerised_text,
+        score: window.confidenceScore,
+      };
 
-    fetch('http://standnote.herokuapp.com/notes/', {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(body),
-    })
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        } else {
-          throw new Error('Something went wrong');
-        }
-      })
-      .then(() => {
-        document.getElementsByClassName('content')[0].style.display = 'none';
-        document.getElementsByClassName('message')[0].style.display = 'block';
-        document.title = 'StandNote - Thanks for using StandNote';
-      })
-      .catch((err) => {
-        document.getElementById('sendText').disabled = false;
+      const res = await fetch('http://standnote.herokuapp.com/notes/', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
       });
+
+      if (!res.ok) {
+        throw new Error('Something went Wrong!');
+      }
+
+      const resData = await res.json();
+
+      document.getElementsByClassName('content')[0].style.display = 'none';
+      document.getElementsByClassName('message')[0].style.display = 'block';
+      document.title = 'StandNote - Thanks for using StandNote';
+    } catch (err) {
+      console.log(err);
+      document.getElementById('sendText').disabled = false;
+
+      document.getElementById('error').innerText =
+        'Error while uploading the data!';
+    }
   });
