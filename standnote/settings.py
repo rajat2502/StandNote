cb455fad3caccfa7f9281872543372efd9293c24
PragatiVerma18 <itispragativerma@gmail.com>
2020-12-12 01:35:42 +0530

    changes email

--- standnote/settings.py
+++ standnote/settings.py
@@ -61,8 +61,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'pragativerma@jssaten.ac.in'
-EMAIL_HOST_PASSWORD = 'iamnotgroot'
+EMAIL_HOST_USER = 'standnote@gmail.com'
+EMAIL_HOST_PASSWORD = 'pragatirocks'
 
 MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
